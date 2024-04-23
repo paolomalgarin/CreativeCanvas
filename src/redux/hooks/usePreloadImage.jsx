371b35946244ae9a4f012019b0e438139947fa45
Pallo <paolomalgarin@gmail.com>
2024-04-23 21:11:04 +0200

    aumentata speed

--- src/redux/hooks/usePreloadImage.jsx
+++ src/redux/hooks/usePreloadImage.jsx
@@ -2,11 +2,25 @@ import { useEffect } from "react";
 
 function usePreloadImage(url) {
   useEffect(() => {
-    const preloadImage = (url) => {
-      const img = new Image();
-      img.src = url;
+    const preloadImage = async (url) => {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => resolve();
+        img.onerror = () => reject();
+        img.src = url;
+      });
     };
-  }, []);
+
+    if (url) {
+      preloadImage(url)
+        .then(() => {
+          console.log(`Image ${url} preloaded successfully`);
+        })
+        .catch(() => {
+          console.error(`Failed to preload image ${url}`);
+        });
+    }
+  }, [url]);
 }
 
 export default usePreloadImage;
